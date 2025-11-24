b278fa11cf20bf13f259e65d53e3aa61bedb5e11
sadman <sadmanhoque@yahoo.com>
2025-11-23 23:32:05 -0500

    testing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from transformers import pipeline
 
-import sys
-print(sys.executable)
 print("-----------------------------")
 model = pipeline("summarization", model="facebook/bart-large-cnn")
 response = model("text to summarize")
