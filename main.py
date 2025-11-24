d02219d4b0f7e57c3db5bf9cae695e4c32561471
sadman <sadmanhoque@yahoo.com>
2025-11-23 23:25:15 -0500

    got rid of temp files

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from transformers import pipeline
 
+import sys
+print(sys.executable)
+print("-----------------------------")
 model = pipeline("summarization", model="facebook/bart-large-cnn")
 response = model("text to summarize")
 print(response)
